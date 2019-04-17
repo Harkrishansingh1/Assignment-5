2785b628e413111d93beb5bc3c86bda4e09d1b82
visualstudio <visualstudio@122-PC24>
2019-04-17 08:24:50 -0400

    Starter Code.

--- Assignment-5/Program.cs
+++ Assignment-5/Program.cs
@@ -10,6 +10,19 @@ namespace Assignment_5
     {
         static void Main(string[] args)
         {
+            { new GenerateList().Run(57); }
+        }
+    }
+    class GenerateList
+    {
+        const int LEN = 100;
+        ArrayList ListQ;
+        public void Run(int AddUpTpNumber)
+        {
+            Random r = new Random();
+            ListQ = new ArrayList();
+            for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
+            this.findTwoNumbersThatAddUpTo(AddUpToNumber);
         }
     }
 }
